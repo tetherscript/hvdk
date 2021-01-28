09d53c46dd84ad67f073869927ff524a8edb337c
Micke <Micke3rd@users.noreply.github.com>
2021-01-28 12:06:25 +0100

    removed unnecessary build fluff

--- CSharp/Keyboard Reader/Properties/AssemblyInfo.cs
+++ CSharp/Keyboard Reader/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mouse Sender Abs")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+
+
+
 [assembly: AssemblyProduct("Mouse Sender Abs")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
